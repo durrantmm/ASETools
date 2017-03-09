00169d85603c1c684b3bfad52232d85feec6e93d
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-08 20:52:17 -0700

    update

---
+++ asetools/pipeline/call_variants_rna.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+from os.path import basename, splitext
+import importlib.machinery
+import subprocess
+import argparse
+
+STAR_VERSION_ERROR = "The STAR aligner is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
+
+
+def main(config):
+    align_reads_with_STAR(config)
+
+def align_reads_with_STAR(config):
+    bash_command = config
+
+def check_star_version(star_path, version, version_flag="--version"):
+    global STAR_VERSION_ERROR
+    stdout = subprocess.check_output([star_path, version_flag])
+    local_version = stdout.split('\n')[0].split()[-1]
+    assert version == local_version, STAR_VERSION_ERROR.format(ACTUAL=local_version, EXPECTED=version)
+
+
+if __name__ == "__main__":
+    config_path = sys.argv[1]
+    config_module_name = splitext(basename(config_path))[0]
+
+    config = importlib.machinery.SourceFileLoader(config_module_name, config_path).load_module().Config
+
+    main(config)
