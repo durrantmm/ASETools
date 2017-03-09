a77859a6b9f03a006b05684132d5ed7233aff918
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-08 21:12:04 -0700

    update

--- asetools/pipeline/call_variants_rna.py
+++ asetools/pipeline/call_variants_rna.py
@@ -7,19 +7,33 @@ import subprocess
 import argparse
 
 STAR_VERSION_ERROR = "The STAR aligner is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
+tab = '\t'
+whitespace = ' '
+new_line = '\n'
+utf8 = "utf-8"
 
 
 def main(config):
     align_reads_with_STAR(config)
 
 def align_reads_with_STAR(config):
+    global whitespace
     check_star_version(config.StarAligner.PATH, config.StarAligner.VERSION)
+    star_command_args = whitespace.join(map(lambda x: whitespace.join(x), config.StarAligner.STAR_COMMAND_DICT.items()))
+    star_command = whitespace.join([config.StarAligner.PATH, star_command_args])
+    print(star_command)
 
+
+
+
+
+
+
+
+# Version Checks
 def check_star_version(star_path, version, version_flag="--version"):
-    global STAR_VERSION_ERROR
-    stdout = subprocess.check_output([star_path, version_flag]).decode("utf-8").strip()
-    print(stdout)
-    local_version = stdout.strip()
+    global STAR_VERSION_ERROR, utf8
+    local_version = subprocess.check_output([star_path, version_flag]).decode(utf8).strip()
     assert version == local_version, STAR_VERSION_ERROR.format(ACTUAL=local_version, EXPECTED=version)
 
 
