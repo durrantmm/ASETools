429eed1c4e4a9eda4578922ceb50cfcd99ff44e1
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-08 22:08:10 -0700

    update

--- asetools/pipeline/call_variants_rna.py
+++ asetools/pipeline/call_variants_rna.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, os
 from os.path import basename, splitext
 import importlib.machinery
 import subprocess
@@ -61,11 +61,9 @@ def contains_iterable(iterable):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                        const=sum, default=max,
-                        help='sum the integers (default: find the max)')
+    parser.add_argument('config', type=os.path.isfile,
+                        help='The config.py file to use in the pipeline.')
+    parser.add_argument('-osa', '--override_star_argument', type=int, action="append", nargs=2)
 
     args = vars(parser.parse_args())
     print(args)
