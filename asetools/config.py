bf666b3f1f0876f7dacf96f97464727f446aeac1
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-09 13:18:46 -0700

    update

--- asetools/config.py
+++ asetools/config.py
@@ -1,78 +1,145 @@
 #!/usr/bin/env python3
 from collections import OrderedDict
+import os
 
 class Config:
+    # Miscellaneous, change at your own riske
+    utf8 = "UTF8"
+    empty_string = ''
+    config_suffix = '.config'
+
     # General reference genome in fasta format. For example, ucsc.hg19.fasta
     REFERENCE_GENOME_PATH = "/srv/gsfs0/projects/bhatt/mdurrant/BUTYRATE_brayon/references/hg19/ucsc.hg19.fasta"
-    
-    class StarAligner:
-    
+
+
+    class PipelineFlow:
+        global Config
+
+        # Output dir, usually specified as a command line argument.
+        MAIN_OUTPUT_DIR = None
+
+        def update_main_path(self, path):
+            PipelineFlow.MAIN_OUTPUT_DIR = os.path.abspath(path)
+
+            # Update CallVariantsRNASeq Paths
+            PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR = os.path.join(
+                PipelineFlow.MAIN_OUTPUT_DIR,
+                os.path.basename(PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR))
+
+            PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS_CONFIG_PATH = os.path.join(
+                PipelineFlow.MAIN_OUTPUT_DIR,
+                PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS_CONFIG_PATH
+            )
+
+            PipelineFlow.CallVariantsRNASeq.PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH = os.path.join(
+                PipelineFlow.MAIN_OUTPUT_DIR,
+                PipelineFlow.CallVariantsRNASeq.PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH
+            )
+
+            PipelineFlow.CallVariantsRNASeq.PICARD_MARK_DUPLICATES_CONFIG_PATH = os.path.join(
+                PipelineFlow.MAIN_OUTPUT_DIR,
+                PipelineFlow.CallVariantsRNASeq.PICARD_MARK_DUPLICATES_CONFIG_PATH
+            )
+
+
+
+        class CallVariantsRNASeq:
+            global Config
+            global PipelineFlow
+
+            OUTPUT_DIR = "call_variants_rna"
+
+            STAR_ALIGN_READS = "align_reads"
+            PICARD_ADD_OR_REPLACE_READ_GROUPS = "add_read_groups"
+            PICARD_MARK_DUPLICATES = "mark_duplicates"
+
+            ORDER = [STAR_ALIGN_READS, PICARD_ADD_OR_REPLACE_READ_GROUPS, PICARD_MARK_DUPLICATES]
+            START = STAR_ALIGN_READS
+
+            STAR_ALIGN_READS_CONFIG_PATH = Config.empty_string.join([STAR_ALIGN_READS, Config.config_suffix])
+            PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH = Config.empty_string.join([PICARD_ADD_OR_REPLACE_READ_GROUPS, Config.config_suffix])
+            PICARD_MARK_DUPLICATES_CONFIG_PATH = Config.empty_string.join([PICARD_MARK_DUPLICATES, Config.config_suffix])
+
+
+
+    class Java:
+        PATH = "/srv/gs1/software/java/jre1.8.0_66/bin/java"
+        VERSION = "1.8.0_66"
+        VERSION_FLAG = "-version"
+        PARSE_VERSION = lambda x: x
+        VERSION_ERROR = "Your java is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
+
+
+    class STAR:
+        global utf8
+
         # Path to the star aligner, absolute path is preferred over aliases
         PATH = "/home/mdurrant/miniconda3/bin/STAR"
         # The version of STAR aligner. Change at your own risk.
-        VERSION = "STAR_2.5.2b"
-    
         # These are the options for the STAR alignment command
         # If the argument is optional, set its value to None to exclude.
+        VERSION = "STAR_2.5.2b"
         VERSION_FLAG = '--version'
-    
+        PARSE_VERSION = lambda x: x.decode(utf8)
+        VERSION_ERROR = "The STAR aligner is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
+
         genomeDir_FLAG = "--genomeDir"
         genomeDir_ARG = "/srv/gsfs0/projects/bhatt/mdurrant/BUTYRATE_brayon/references/starGenomeUCSChg19"
-    
+
         readFilesIn_FLAG = "--readFilesIn"
         readFilesIn_ARG = None
-    
+
         readFilesCommand_FLAG = "--readFilesCommand"
         readFilesCommand_ARG = "zcat"
-    
+
         runThreadN_FLAG = "--runThreadN"
         runThreadN_ARG = 2
-    
+
         genomeLoad_FLAG = "--genomeLoad"
         genomeLoad_ARG = "NoSharedMemory"
-    
+
         outFilterMultimapNmax_FLAG = "--outFilterMultimapNmax"
         outFilterMultimapNmax_ARG = 20
-    
+
         alignSJDBoverhangMin_FLAG = "--alignSJDBoverhangMin"
         alignSJDBoverhangMin_ARG = 1
-    
+
         outFilterMismatchNmax_FLAG = "--outFilterMultimapNmax"
         outFilterMismatchNmax_ARG = 999
-    
+
         outFilterMismatchNoverReadLmax_FLAG = "--outFilterMismatchNoverReadLmax"
         outFilterMismatchNoverReadLmax_ARG = 0.04
-    
+
         alignIntronMin_FLAG = "--alignIntronMin"
         alignIntronMin_ARG = 20
-    
+
         alignIntronMax_FLAG = "--alignIntronMax"
         alignIntronMax_ARG = 1000000
-    
+
         alignMatesGapMax_FLAG = "--alignMatesGapMax"
         alignMatesGapMax_ARG = 1000000
-    
+
         outSAMunmapped_FLAG = "--outSAMunmapped"
         outSAMunmapped_ARG = "Within"
-    
+
         outFilterType_FLAG = "--outFilterType"
         outFilterType_ARG = "BySJout"
-    
+
         outSAMattributes_FLAG = "--outSAMattributes"
         outSAMattributes_ARG = ['NH', 'HI', 'AS', 'NM', 'MD']
-    
+
         sjdbScore_FLAG = "--sjdbScore"
         sjdbScore_ARG = 1
-    
+
         twopassMode_FLAG = "--twopassMode"
         twopassMode_ARG = "Basic"
-    
+
         twopass1readsN_FLAG = "--twopass1readsN"
         twopass1readsN_ARG = -1
-    
+
         outFileNamePrefix_FLAG = "--outFileNamePrefix"
         outFileNamePrefix_ARG = None
-    
+
         STAR_COMMAND_DICT = OrderedDict([(genomeDir_FLAG, genomeDir_ARG),
                                          (readFilesIn_FLAG, readFilesIn_ARG),
                                          (readFilesCommand_FLAG, readFilesCommand_ARG),
@@ -92,3 +159,45 @@ class Config:
                                          (twopassMode_FLAG, twopassMode_ARG),
                                          (twopass1readsN_FLAG, twopass1readsN_ARG),
                                          (outFileNamePrefix_FLAG, outFileNamePrefix_ARG)])
+
+    class Picard:
+
+        PATH = "/srv/gs1/software/picard-tools/2.8.0/picard.jar"
+
+
+        class AddOrReplaceReadGroups:
+
+            INPUT_FLAG = "I="
+            INPUT_ARG = None
+
+            OUTPUT_FLAG = "O="
+            OUTPUT_ARG = None
+
+            SORT_ORDER_FLAG = "SO="
+            SORT_ORDER_ARG = "coordinate"
+
+            RGID_FLAG = "RGID="
+            RGID_ARG = "id"
+
+            RGLB_FLAG = "RGLB="
+            RGLB_ARG = "library"
+
+            RGPL_FLAG = "RGPL="
+            RGPL_ARG = "platform"
+
+            RGPU_FLAG = "RGPU="
+            RGPU_ARG = "machine"
+
+            RGSM_FLAG = "RGSM="
+            RGSM_ARG = "sample"
+
+        class MarkDuplicates:
+            java - jar
+            picard.jar
+            MarkDuplicates
+            I = rg_added_sorted.bam
+            O = dedupped.bam
+            CREATE_INDEX = true
+            VALIDATION_STRINGENCY = SILENT
+            M = output.metrics
+
--- asetools/pipeline/call_variants_rna.py
+++ asetools/pipeline/call_variants_rna.py
@@ -5,30 +5,61 @@ from os.path import basename, splitext
 import importlib.machinery
 import subprocess
 import argparse
+from collections import namedtuple
+from glob import glob
+import json
 
-STAR_VERSION_ERROR = "The STAR aligner is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
 tab = '\t'
 whitespace = ' '
 new_line = '\n'
 utf8 = "utf-8"
 
+STAR_ALIGN_READS = "align_reads"
+PICARD_ADD_OR_REPLACE_READ_GROUPS = "add_read_groups"
+PICARD_MARK_DUPLICATES = "mark_duplicates"
 
-def main(config):
-    align_reads_with_STAR(config)
 
-def align_reads_with_STAR(config):
-    check_star_version(config.StarAligner.PATH, config.StarAligner.VERSION)
-    star_command_args = format_command_args(config.StarAligner.STAR_COMMAND_DICT)
-    star_command = whitespace.join([config.StarAligner.PATH, star_command_args])
-    print(star_command)
-    output = subprocess.check_output(star_command.split()).decode(utf8).strip()
-    print(output)
+
+def run(config):
+
+    os.makedirs(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR, exist_ok=True)
+
+    pipeline_start = config.PipelineFlow.CallVariantsRNASeq.START
+    pipeline_order = config.PipelineFlow.CallVariantsRNASeq.ORDER
+
+    if run_pipeline_step(pipeline_start, STAR_ALIGN_READS, pipeline_order):
+        config = align_reads_STAR(config)
+        write_config_align_reads_STAR(config)
+
+def align_reads_STAR(config):
+    if config.STAR.outFileNamePrefix_ARG:
+        config.STAR.outFileNamePrefix_ARG = os.path.join(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR,
+                                                         config.STAR.outFileNamePrefix_ARG)
+    else:
+        config.STAR.outFileNamePrefix_ARG = os.path.join(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR,
+                                                         basename(config.PipelineFlow.OUTPUT_DIR))
+
+    check_version(config.STAR.PATH, config.STAR.VERSION, config.STAR.PARSE_VERSION, config.STAR.VERSION_ERROR)
+    star_command_args = format_command_args(config.STAR.STAR_COMMAND_DICT)
+    star_command = whitespace.join([config.STAR.PATH, star_command_args])
+    subprocess.check_output(star_command.split()).decode(utf8).strip()
+    return config
+
+def write_config_align_reads_STAR(config):
+    global tab, newline
+    out_config = lambda: None
+    out_config.output_prefix = config.STAR.outFileNamePrefix_ARG
+    out_config_dict = out_config.__dict__()
+    with open(config.PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS_CONFIG_PATH, 'w') as outfile:
+        outfile.write(json.dumps(out_config_dict, indent=4))
+
 
 
 # Version Checks
-def check_star_version(star_path, version, version_flag="--version"):
-    local_version = subprocess.check_output([star_path, version_flag]).decode(utf8).strip()
-    assert version == local_version, STAR_VERSION_ERROR.format(ACTUAL=local_version, EXPECTED=version)
+def check_version(star_path, version, parse_version, version_error, version_flag="--version"):
+    output = subprocess.check_output([star_path, version_flag])
+    local_version = parse_version(output)
+    assert version == local_version, version_error.format(ACTUAL=local_version, EXPECTED=version)
 
 
 # Misc functions
@@ -44,44 +75,73 @@ def format_command_args(command_args, delim=whitespace):
     return delim.join(out_command)
 
 
-def contains_iterable(iterable):
-    if not iterable:
+def run_pipeline_step(start, step, order):
+    if order.index(start) <= order.index(step):
+        return True
+    else:
         return False
 
-    if isinstance(iterable, str):
-        return False
 
-    if not hasattr(iterable, '__iter__'):
-        return False
-
-    for item in iterable:
-        if hasattr(iterable, '__iter__'):
-            return True
-
-    return False
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('config', type=str,
-                        help='The config.py file to use in the pipeline.')
-    parser.add_argument('-oSTAR', '--override_star_argument', action="append",
-                        help="Onlye one new argument per use of option. "
-                             "Example: -oSTAR \"--readFilesIn paired_ends.1.fq.gz paired_ends.2.fq.gz\""
-                             "-oSTAR \"--out1FileNamePrefix paired_end_alignment\"")
-
-    args = vars(parser.parse_args())
-
-    config_path = args['config']
+def get_config(config_path):
     config_module_name = splitext(basename(config_path))[0]
-
     config = importlib.machinery.SourceFileLoader(config_module_name, config_path).load_module().Config
 
-    if args["override_star_argument"]:
-        for entry in args["override_star_argument"]:
+
+def dict_to_namedtuple(dict_in):
+    return namedtuple('GenericDict', dict_in.keys())(**dict_in)
+
+
+def adjust_config_by_args(config, args):
+    # Adding the override arguments
+    if args.override_star_argument:
+        for entry in args.override_star_argument:
             flag, argument = entry.split()[0], whitespace.join(entry.split()[1:])
             config.StarAligner.STAR_COMMAND_DICT[flag] = argument
 
-    main(config)
+    # Setting the start position
+    config.PipelineFlow.CallVariantsRNASeq.START = args.start
+
+    # Setting the main output dir
+    config.PipelineFlow.OUTPUT_DIR = args.output_dir
+
+    return config
+
+
+def arrange_output_dir(output_dir, config):
+    config.PipelineFlow.update_main_output_path(output_dir)
+    return config
+
+
+def argument_parser():
+
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('config', type=str,
+                        help='The config.py file to use in the pipeline.')
+    parser.add_argument('output', type=str,
+                        help='The output folder to be used by the pipeline.')
+    args_first_parse = vars(parser.parse_args())
+    config = get_config(args_first_parse.config)
+
+    parser.add_argument('-oSTAR', '--override_star_argument', action="append",
+                        help="Only one new argument for each time you invoke the option."
+                             "Example: -oSTAR \"--readFilesIn paired_ends.1.fq.gz paired_ends.2.fq.gz\""
+                             "-oSTAR \"--out1FileNamePrefix paired_end_alignment\"")
+
+    parser.add_argument('-s', '--start', choices=config.PipelineFlow.CallVariantsRNASeq.ORDER,
+                        default=config.PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS, type=str,
+                        help="Choose the point in the call variants pipeline at which you'd like to start.")
+
+    args = vars(parser.parse_args())
+    args = dict_to_namedtuple(args)
+
+    config = adjust_config_by_args(config, args)
+    config = arrange_output_dir(args.output, config)
+
+    return config
+
+if __name__ == "__main__":
+    config = argument_parser()
+
+    run(config)
 
 
