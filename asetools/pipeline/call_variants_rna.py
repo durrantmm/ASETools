fe3e2e3493fa23ad23228008f62b0e2a9c274bd2
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-08 22:54:47 -0700

    update

--- asetools/pipeline/call_variants_rna.py
+++ asetools/pipeline/call_variants_rna.py
@@ -20,7 +20,8 @@ def align_reads_with_STAR(config):
     check_star_version(config.StarAligner.PATH, config.StarAligner.VERSION)
     star_command_args = format_command_args(config.StarAligner.STAR_COMMAND_DICT)
     star_command = whitespace.join([config.StarAligner.PATH, star_command_args])
-    print(star_command)
+    output = subprocess.check_output(star_command.split()).decode(utf8).strip()
+    print(output)
 
 
 # Version Checks
