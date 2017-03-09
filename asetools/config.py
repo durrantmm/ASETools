81848aed751bf336e9c115975c85b5fcfd03a2b4
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-09 15:01:34 -0700

    update

--- asetools/config.py
+++ asetools/config.py
@@ -2,202 +2,211 @@
 from collections import OrderedDict
 import os
 
+### Global attributes and methods accessible to all classes
+STR_CONST = type("StringConstants", (),
+                 {'UTF8':'utf-8',
+                  'EMPTY_STRING': '',
+                  'CONFIG_SUFFIX': '.config'})
+
+REFERENCE_GENOME_PATH = "/srv/gsfs0/projects/bhatt/mdurrant/BUTYRATE_brayon/references/hg19/ucsc.hg19.fasta"
+
+
 class Config:
-    # Miscellaneous, change at your own riske
-    utf8 = "UTF8"
-    empty_string = ''
-    config_suffix = '.config'
+    def __init__(self):
+        self.PipelineFlow = PipelineFlow(self)
+        self.RunSTAR = RunSTAR()
 
-    # General reference genome in fasta format. For example, ucsc.hg19.fasta
-    REFERENCE_GENOME_PATH = "/srv/gsfs0/projects/bhatt/mdurrant/BUTYRATE_brayon/references/hg19/ucsc.hg19.fasta"
+        # General reference genome in fasta format. For example, ucsc.hg19.fasta
 
 
-    class PipelineFlow:
-        global Config
+class PipelineFlow:
+
+    def __init__(self, config_in):
+        self.Config = config_in
+        self.CallVariantsRNASeq = PipelineCallVariantsRNASeq(self)
 
         # Output dir, usually specified as a command line argument.
-        MAIN_OUTPUT_DIR = None
+        self.MAIN_OUTPUT_DIR = None
 
-        def update_main_path(self, path):
-            PipelineFlow.MAIN_OUTPUT_DIR = os.path.abspath(path)
+    def update_main_output_path(self, path):
+        self.MAIN_OUTPUT_DIR = os.path.abspath(path)
+        os.makedirs(self.MAIN_OUTPUT_DIR, exist_ok=True)
 
-            # Update CallVariantsRNASeq Paths
-            PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR = os.path.join(
-                PipelineFlow.MAIN_OUTPUT_DIR,
-                os.path.basename(PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR))
-
-            PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS_CONFIG_PATH = os.path.join(
-                PipelineFlow.MAIN_OUTPUT_DIR,
-                PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS_CONFIG_PATH
-            )
-
-            PipelineFlow.CallVariantsRNASeq.PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH = os.path.join(
-                PipelineFlow.MAIN_OUTPUT_DIR,
-                PipelineFlow.CallVariantsRNASeq.PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH
-            )
-
-            PipelineFlow.CallVariantsRNASeq.PICARD_MARK_DUPLICATES_CONFIG_PATH = os.path.join(
-                PipelineFlow.MAIN_OUTPUT_DIR,
-                PipelineFlow.CallVariantsRNASeq.PICARD_MARK_DUPLICATES_CONFIG_PATH
-            )
+        self.CallVariantsRNASeq.update_paths_relative(path)
 
 
+class PipelineCallVariantsRNASeq:
+    global STR_CONST
 
-        class CallVariantsRNASeq:
-            global Config
-            global PipelineFlow
+    def __init__(self, pipeline_flow_in):
+        self.PipelineFlow = pipeline_flow_in
 
-            OUTPUT_DIR = "call_variants_rna"
+        self.OUTPUT_DIR = "call_variants_rna"
 
-            STAR_ALIGN_READS = "align_reads"
-            PICARD_ADD_OR_REPLACE_READ_GROUPS = "add_read_groups"
-            PICARD_MARK_DUPLICATES = "mark_duplicates"
+        self.STAR_ALIGN_READS = "align_reads"
+        self.PICARD_ADD_OR_REPLACE_READ_GROUPS = "add_read_groups"
+        self.PICARD_MARK_DUPLICATES = "mark_duplicates"
 
-            ORDER = [STAR_ALIGN_READS, PICARD_ADD_OR_REPLACE_READ_GROUPS, PICARD_MARK_DUPLICATES]
-            START = STAR_ALIGN_READS
-
-            STAR_ALIGN_READS_CONFIG_PATH = Config.empty_string.join([STAR_ALIGN_READS, Config.config_suffix])
-            PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH = Config.empty_string.join([PICARD_ADD_OR_REPLACE_READ_GROUPS, Config.config_suffix])
-            PICARD_MARK_DUPLICATES_CONFIG_PATH = Config.empty_string.join([PICARD_MARK_DUPLICATES, Config.config_suffix])
+        self.ORDER = [self.STAR_ALIGN_READS, self.PICARD_ADD_OR_REPLACE_READ_GROUPS, self.PICARD_MARK_DUPLICATES]
+        self.START = self.STAR_ALIGN_READS
 
 
+    def update_paths_relative(self, main_output_dir):
 
-    class Java:
-        PATH = "/srv/gs1/software/java/jre1.8.0_66/bin/java"
-        VERSION = "1.8.0_66"
-        VERSION_FLAG = "-version"
-        PARSE_VERSION = lambda x: x
-        VERSION_ERROR = "Your java is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
+        self.OUTPUT_DIR = os.path.join(
+            main_output_dir, self.OUTPUT_DIR)
+        os.makedirs(self.OUTPUT_DIR, exist_ok=True)
+
+        self.STAR_ALIGN_READS_CONFIG_PATH = os.path.join(
+            main_output_dir, self.STAR_ALIGN_READS+STR_CONST.CONFIG_SUFFIX)
+        os.makedirs(self.STAR_ALIGN_READS_CONFIG_PATH, exist_ok=True)
+
+        self.PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH = os.path.join(
+            main_output_dir, self.PICARD_ADD_OR_REPLACE_READ_GROUPS + STR_CONST.CONFIG_SUFFIX)
+        os.makedirs(self.PICARD_ADD_OR_REPLACE_READ_GROUPS_CONFIG_PATH, exist_ok=True)
+
+        self.PICARD_MARK_DUPLICATES_CONFIG_PATH = os.path.join(
+            main_output_dir, self.PICARD_MARK_DUPLICATES+STR_CONST.CONFIG_SUFFIX)
+        os.makedirs(self.PICARD_MARK_DUPLICATES_CONFIG_PATH, exist_ok=True)
 
 
-    class STAR:
-        global utf8
+class RunSTAR:
 
+    def __init__(self):
         # Path to the star aligner, absolute path is preferred over aliases
-        PATH = "/home/mdurrant/miniconda3/bin/STAR"
+        self.PATH = "/home/mdurrant/miniconda3/bin/STAR"
         # The version of STAR aligner. Change at your own risk.
         # These are the options for the STAR alignment command
         # If the argument is optional, set its value to None to exclude.
-        VERSION = "STAR_2.5.2b"
-        VERSION_FLAG = '--version'
-        PARSE_VERSION = lambda x: x.decode(utf8)
-        VERSION_ERROR = "The STAR aligner is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
+        self.VERSION = "STAR_2.5.2b"
+        self.VERSION_FLAG = '--version'
+        self.PARSE_VERSION = lambda x: x.decode(STR_CONST.UTF8)
+        self.VERSION_ERROR = "The STAR aligner is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
 
-        genomeDir_FLAG = "--genomeDir"
-        genomeDir_ARG = "/srv/gsfs0/projects/bhatt/mdurrant/BUTYRATE_brayon/references/starGenomeUCSChg19"
+        self.genomeDir_FLAG = "--genomeDir"
+        self.genomeDir_ARG = "/srv/gsfs0/projects/bhatt/mdurrant/BUTYRATE_brayon/references/starGenomeUCSChg19"
 
-        readFilesIn_FLAG = "--readFilesIn"
-        readFilesIn_ARG = None
+        self.readFilesIn_FLAG = "--readFilesIn"
+        self.readFilesIn_ARG = None
 
-        readFilesCommand_FLAG = "--readFilesCommand"
-        readFilesCommand_ARG = "zcat"
+        self.readFilesCommand_FLAG = "--readFilesCommand"
+        self.readFilesCommand_ARG = "zcat"
 
-        runThreadN_FLAG = "--runThreadN"
-        runThreadN_ARG = 2
+        self.runThreadN_FLAG = "--runThreadN"
+        self.runThreadN_ARG = 2
 
-        genomeLoad_FLAG = "--genomeLoad"
-        genomeLoad_ARG = "NoSharedMemory"
+        self.genomeLoad_FLAG = "--genomeLoad"
+        self.genomeLoad_ARG = "NoSharedMemory"
 
-        outFilterMultimapNmax_FLAG = "--outFilterMultimapNmax"
-        outFilterMultimapNmax_ARG = 20
+        self.outFilterMultimapNmax_FLAG = "--outFilterMultimapNmax"
+        self.outFilterMultimapNmax_ARG = 20
 
-        alignSJDBoverhangMin_FLAG = "--alignSJDBoverhangMin"
-        alignSJDBoverhangMin_ARG = 1
+        self.alignSJDBoverhangMin_FLAG = "--alignSJDBoverhangMin"
+        self.alignSJDBoverhangMin_ARG = 1
 
-        outFilterMismatchNmax_FLAG = "--outFilterMultimapNmax"
-        outFilterMismatchNmax_ARG = 999
+        self.outFilterMismatchNmax_FLAG = "--outFilterMultimapNmax"
+        self.outFilterMismatchNmax_ARG = 999
 
-        outFilterMismatchNoverReadLmax_FLAG = "--outFilterMismatchNoverReadLmax"
-        outFilterMismatchNoverReadLmax_ARG = 0.04
+        self.outFilterMismatchNoverReadLmax_FLAG = "--outFilterMismatchNoverReadLmax"
+        self.outFilterMismatchNoverReadLmax_ARG = 0.04
 
-        alignIntronMin_FLAG = "--alignIntronMin"
-        alignIntronMin_ARG = 20
+        self.alignIntronMin_FLAG = "--alignIntronMin"
+        self.alignIntronMin_ARG = 20
 
-        alignIntronMax_FLAG = "--alignIntronMax"
-        alignIntronMax_ARG = 1000000
+        self.alignIntronMax_FLAG = "--alignIntronMax"
+        self.alignIntronMax_ARG = 1000000
 
-        alignMatesGapMax_FLAG = "--alignMatesGapMax"
-        alignMatesGapMax_ARG = 1000000
+        self.alignMatesGapMax_FLAG = "--alignMatesGapMax"
+        self.alignMatesGapMax_ARG = 1000000
 
-        outSAMunmapped_FLAG = "--outSAMunmapped"
-        outSAMunmapped_ARG = "Within"
+        self.outSAMunmapped_FLAG = "--outSAMunmapped"
+        self.outSAMunmapped_ARG = "Within"
 
-        outFilterType_FLAG = "--outFilterType"
-        outFilterType_ARG = "BySJout"
+        self.outFilterType_FLAG = "--outFilterType"
+        self.outFilterType_ARG = "BySJout"
 
-        outSAMattributes_FLAG = "--outSAMattributes"
-        outSAMattributes_ARG = ['NH', 'HI', 'AS', 'NM', 'MD']
+        self.outSAMattributes_FLAG = "--outSAMattributes"
+        self.outSAMattributes_ARG = ['NH', 'HI', 'AS', 'NM', 'MD']
 
-        sjdbScore_FLAG = "--sjdbScore"
-        sjdbScore_ARG = 1
+        self.sjdbScore_FLAG = "--sjdbScore"
+        self.sjdbScore_ARG = 1
 
-        twopassMode_FLAG = "--twopassMode"
-        twopassMode_ARG = "Basic"
+        self.twopassMode_FLAG = "--twopassMode"
+        self.twopassMode_ARG = "Basic"
 
-        twopass1readsN_FLAG = "--twopass1readsN"
-        twopass1readsN_ARG = -1
+        self.twopass1readsN_FLAG = "--twopass1readsN"
+        self.twopass1readsN_ARG = -1
 
-        outFileNamePrefix_FLAG = "--outFileNamePrefix"
-        outFileNamePrefix_ARG = None
+        self.outFileNamePrefix_FLAG = "--outFileNamePrefix"
+        self.outFileNamePrefix_ARG = None
 
-        STAR_COMMAND_DICT = OrderedDict([(genomeDir_FLAG, genomeDir_ARG),
-                                         (readFilesIn_FLAG, readFilesIn_ARG),
-                                         (readFilesCommand_FLAG, readFilesCommand_ARG),
-                                         (runThreadN_FLAG, runThreadN_ARG),
-                                         (genomeLoad_FLAG, genomeLoad_ARG),
-                                         (outFilterMultimapNmax_FLAG, outFilterMultimapNmax_ARG),
-                                         (alignSJDBoverhangMin_FLAG, alignSJDBoverhangMin_ARG),
-                                         (outFilterMismatchNmax_FLAG, outFilterMismatchNmax_ARG),
-                                         (outFilterMismatchNoverReadLmax_FLAG, outFilterMismatchNoverReadLmax_ARG),
-                                         (alignIntronMin_FLAG, alignIntronMin_ARG),
-                                         (alignIntronMax_FLAG, alignIntronMax_ARG),
-                                         (alignMatesGapMax_FLAG, alignMatesGapMax_ARG),
-                                         (outSAMunmapped_FLAG, outSAMunmapped_ARG),
-                                         (outFilterType_FLAG, outFilterType_ARG),
-                                         (outSAMattributes_FLAG, outSAMattributes_ARG),
-                                         (sjdbScore_FLAG, sjdbScore_ARG),
-                                         (twopassMode_FLAG, twopassMode_ARG),
-                                         (twopass1readsN_FLAG, twopass1readsN_ARG),
-                                         (outFileNamePrefix_FLAG, outFileNamePrefix_ARG)])
+        self.STAR_COMMAND_DICT = OrderedDict([(self.genomeDir_FLAG, self.genomeDir_ARG),
+                                              (self.readFilesIn_FLAG, self.readFilesIn_ARG),
+                                              (self.readFilesCommand_FLAG, self.readFilesCommand_ARG),
+                                              (self.runThreadN_FLAG, self.runThreadN_ARG),
+                                              (self.genomeLoad_FLAG, self.genomeLoad_ARG),
+                                              (self.outFilterMultimapNmax_FLAG, self.outFilterMultimapNmax_ARG),
+                                              (self.alignSJDBoverhangMin_FLAG, self.alignSJDBoverhangMin_ARG),
+                                              (self.outFilterMismatchNmax_FLAG, self.outFilterMismatchNmax_ARG),
+                                              (self.outFilterMismatchNoverReadLmax_FLAG,
+                                               self.outFilterMismatchNoverReadLmax_ARG),
+                                              (self.alignIntronMin_FLAG, self.alignIntronMin_ARG),
+                                              (self.alignIntronMax_FLAG, self.alignIntronMax_ARG),
+                                              (self.alignMatesGapMax_FLAG, self.alignMatesGapMax_ARG),
+                                              (self.outSAMunmapped_FLAG, self.outSAMunmapped_ARG),
+                                              (self.outFilterType_FLAG, self.outFilterType_ARG),
+                                              (self.outSAMattributes_FLAG, self.outSAMattributes_ARG),
+                                              (self.sjdbScore_FLAG, self.sjdbScore_ARG),
+                                              (self.twopassMode_FLAG, self.twopassMode_ARG),
+                                              (self.twopass1readsN_FLAG, self.twopass1readsN_ARG),
+                                              (self.outFileNamePrefix_FLAG, self.outFileNamePrefix_ARG)])
 
-    class Picard:
+    def set_out_prefix(self, override=False):
+        if config.RunSTAR.outFileNamePrefix_ARG:
+            config.RunSTAR.outFileNamePrefix_ARG = os.path.join(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR,
+                                                                config.RunSTAR.outFileNamePrefix_ARG)
+        else:
+            config.RunSTAR.outFileNamePrefix_ARG = os.path.join(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR,
+                                                                basename(config.PipelineFlow.OUTPUT_DIR))
 
-        PATH = "/srv/gs1/software/picard-tools/2.8.0/picard.jar"
+
+class Java:
+    PATH = "/srv/gs1/software/java/jre1.8.0_66/bin/java"
+    VERSION = "1.8.0_66"
+    VERSION_FLAG = "-version"
+    PARSE_VERSION = lambda x: x
+    VERSION_ERROR = "Your java is version {ACTUAL}, not {EXPECTED}, as specified in the config file."
 
 
-        class AddOrReplaceReadGroups:
+class RunPicard:
+    PATH = "/srv/gs1/software/picard-tools/2.8.0/picard.jar"
 
-            INPUT_FLAG = "I="
-            INPUT_ARG = None
 
-            OUTPUT_FLAG = "O="
-            OUTPUT_ARG = None
+class RunPicardAddOrReplaceReadGroups:
+    INPUT_FLAG = "I="
+    INPUT_ARG = None
 
-            SORT_ORDER_FLAG = "SO="
-            SORT_ORDER_ARG = "coordinate"
+    OUTPUT_FLAG = "O="
+    OUTPUT_ARG = None
 
-            RGID_FLAG = "RGID="
-            RGID_ARG = "id"
+    SORT_ORDER_FLAG = "SO="
+    SORT_ORDER_ARG = "coordinate"
 
-            RGLB_FLAG = "RGLB="
-            RGLB_ARG = "library"
+    RGID_FLAG = "RGID="
+    RGID_ARG = "id"
 
-            RGPL_FLAG = "RGPL="
-            RGPL_ARG = "platform"
+    RGLB_FLAG = "RGLB="
+    RGLB_ARG = "library"
 
-            RGPU_FLAG = "RGPU="
-            RGPU_ARG = "machine"
+    RGPL_FLAG = "RGPL="
+    RGPL_ARG = "platform"
 
-            RGSM_FLAG = "RGSM="
-            RGSM_ARG = "sample"
+    RGPU_FLAG = "RGPU="
+    RGPU_ARG = "machine"
 
-        class MarkDuplicates:
-            java - jar
-            picard.jar
-            MarkDuplicates
-            I = rg_added_sorted.bam
-            O = dedupped.bam
-            CREATE_INDEX = true
-            VALIDATION_STRINGENCY = SILENT
-            M = output.metrics
+    RGSM_FLAG = "RGSM="
+    RGSM_ARG = "sample"
 
+
+class RunPicardMarkDuplicates:
+    pass
--- asetools/pipeline/call_variants_rna.py
+++ asetools/pipeline/call_variants_rna.py
@@ -9,15 +9,16 @@ from collections import namedtuple
 from glob import glob
 import json
 
-tab = '\t'
-whitespace = ' '
-new_line = '\n'
-utf8 = "utf-8"
+### Global attributes and methods accessible to all classes
+STR_CONST = type("StringConstants", (),
+                 {'UTF8':'utf-8',
+                  'EMPTY_STRING': '',
+                  'NEW_LINE': '\n',
+                  'SPACE': ' '})
 
 STAR_ALIGN_READS = "align_reads"
 PICARD_ADD_OR_REPLACE_READ_GROUPS = "add_read_groups"
 PICARD_MARK_DUPLICATES = "mark_duplicates"
-
 
 
 def run(config):
@@ -32,17 +33,13 @@ def run(config):
         write_config_align_reads_STAR(config)
 
 def align_reads_STAR(config):
-    if config.STAR.outFileNamePrefix_ARG:
-        config.STAR.outFileNamePrefix_ARG = os.path.join(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR,
-                                                         config.STAR.outFileNamePrefix_ARG)
-    else:
-        config.STAR.outFileNamePrefix_ARG = os.path.join(config.PipelineFlow.CallVariantsRNASeq.OUTPUT_DIR,
-                                                         basename(config.PipelineFlow.OUTPUT_DIR))
+    global STR_CONST
 
-    check_version(config.STAR.PATH, config.STAR.VERSION, config.STAR.PARSE_VERSION, config.STAR.VERSION_ERROR)
-    star_command_args = format_command_args(config.STAR.STAR_COMMAND_DICT)
-    star_command = whitespace.join([config.STAR.PATH, star_command_args])
-    subprocess.check_output(star_command.split()).decode(utf8).strip()
+    check_version(config.RunSTAR.PATH, config.RunSTAR.VERSION, config.RunSTAR.PARSE_VERSION, config.RunSTAR.VERSION_ERROR)
+    star_command_args = format_command_args(config.RunSTAR.STAR_COMMAND_DICT)
+    star_command = STR_CONST.SPACE.join([config.STAR.PATH, star_command_args])
+    subprocess.check_output(star_command.split()).decode(STR_CONST.UTF8).strip()
+
     return config
 
 def write_config_align_reads_STAR(config):
@@ -63,7 +60,7 @@ def check_version(star_path, version, parse_version, version_error, version_flag
 
 
 # Misc functions
-def format_command_args(command_args, delim=whitespace):
+def format_command_args(command_args, delim=STR_CONST.SPACE):
     out_command = []
     for key, value in command_args.items():
         if not value:
@@ -82,63 +79,58 @@ def run_pipeline_step(start, step, order):
         return False
 
 
-def get_config(config_path):
-    config_module_name = splitext(basename(config_path))[0]
-    config = importlib.machinery.SourceFileLoader(config_module_name, config_path).load_module().Config
-
-
-def dict_to_namedtuple(dict_in):
-    return namedtuple('GenericDict', dict_in.keys())(**dict_in)
-
-
-def adjust_config_by_args(config, args):
-    # Adding the override arguments
-    if args.override_star_argument:
-        for entry in args.override_star_argument:
-            flag, argument = entry.split()[0], whitespace.join(entry.split()[1:])
-            config.StarAligner.STAR_COMMAND_DICT[flag] = argument
-
-    # Setting the start position
-    config.PipelineFlow.CallVariantsRNASeq.START = args.start
-
-    # Setting the main output dir
-    config.PipelineFlow.OUTPUT_DIR = args.output_dir
-
-    return config
-
-
-def arrange_output_dir(output_dir, config):
-    config.PipelineFlow.update_main_output_path(output_dir)
-    return config
-
-
-def argument_parser():
-
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('config', type=str,
-                        help='The config.py file to use in the pipeline.')
-    parser.add_argument('output', type=str,
-                        help='The output folder to be used by the pipeline.')
-    args_first_parse = dict_to_namedtuple(vars(parser.parse_args()))
-    config = get_config(args_first_parse.config)
-
-    parser.add_argument('-oSTAR', '--override_star_argument', action="append",
-                        help="Only one new argument for each time you invoke the option."
-                             "Example: -oSTAR \"--readFilesIn paired_ends.1.fq.gz paired_ends.2.fq.gz\""
-                             "-oSTAR \"--out1FileNamePrefix paired_end_alignment\"")
-
-    parser.add_argument('-s', '--start', choices=config.PipelineFlow.CallVariantsRNASeq.ORDER,
-                        default=config.PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS, type=str,
-                        help="Choose the point in the call variants pipeline at which you'd like to start.")
-
-    args = dict_to_namedtuple(vars(parser.parse_args()))
-
-    config = adjust_config_by_args(config, args)
-    config = arrange_output_dir(args.output, config)
-
-    return config
-
 if __name__ == "__main__":
+
+    def argument_parser():
+
+        parser = argparse.ArgumentParser(description='Process some integers.')
+        parser.add_argument('config', type=str,
+                            help='The config.py file to use in the pipeline.')
+        parser.add_argument('output', type=str,
+                            help='The output folder to be used by the pipeline.')
+        args_first_parse = parser.parse_args()
+
+        config = get_config(args_first_parse.config)
+
+        parser.add_argument('-oSTAR', '--override_star_argument', action="append",
+                            help="Only one new argument for each time you invoke the option."
+                                 "Example: -oSTAR \"--readFilesIn paired_ends.1.fq.gz paired_ends.2.fq.gz\""
+                                 "-oSTAR \"--out1FileNamePrefix paired_end_alignment\"")
+
+        parser.add_argument('-s', '--start', choices=config.PipelineFlow.CallVariantsRNASeq.ORDER,
+                            default=config.PipelineFlow.CallVariantsRNASeq.STAR_ALIGN_READS, type=str,
+                            help="Choose the point in the call variants pipeline at which you'd like to start.")
+
+        args = parser.parse_args()
+        config = adjust_config(config, args)
+
+        return config
+
+
+    def get_config(config_path):
+        config_module_name = splitext(basename(config_path))[0]
+        config = importlib.machinery.SourceFileLoader(config_module_name, config_path).load_module().Config()
+        return config
+
+
+    def adjust_config(config, args):
+        global STR_CONST
+        # Adding the override arguments
+        if args.override_star_argument:
+            for entry in args.override_star_argument:
+                flag, argument = entry.split()[0], STR_CONST.SPACE.join(entry.split()[1:])
+                config.RunSTAR.STAR_COMMAND_DICT[flag] = argument
+
+        # Setting the start position
+        config.PipelineFlow.CallVariantsRNASeq.START = args.start
+
+        # Setting the main output dir
+        config.PipelineFlow.update_main_output_path(args.output)
+        config.PipelineFlow.OUTPUT_DIR = args.output
+
+        return config
+
+
     config = argument_parser()
 
     run(config)
