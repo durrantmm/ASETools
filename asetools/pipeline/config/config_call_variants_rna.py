78db976c474e23fc9aa0449553e292dd498f6c99
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-10 12:53:38 -0700

    update

--- asetools/pipeline/config/config_call_variants_rna.py
+++ asetools/pipeline/config/config_call_variants_rna.py
@@ -278,7 +278,7 @@ def adjust_path_relative(relative_path, path):
 
 def antijoin_paths(short_path, long_path):
     short_path = os.pathsep.split(short_path)
-    long_path = os.pathstep.split(long_path)
+    long_path = os.pathsep.split(long_path)
 
     trimmed_path = []
     for elem in reversed(short_path):
