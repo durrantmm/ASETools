495e7ab338c587156ca8743b4cbfc85acccd2cd5
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-09 19:26:59 -0700

    update

--- asetools/pipeline/config/config_call_variants_rna.py
+++ asetools/pipeline/config/config_call_variants_rna.py
@@ -34,8 +34,7 @@ class CallVariantsRNAConfig:
 
     def save_config_state(self):
         out_config_dict = self.json_serialize(self)
-        print(out_config_dict)
-        sys.exit()
+
         with open(self.RunSTAR.STAR_ALIGN_READS_CONFIG_PATH, 'w') as outfile:
             outfile.write(json.dumps(out_config_dict, indent=4))
 
