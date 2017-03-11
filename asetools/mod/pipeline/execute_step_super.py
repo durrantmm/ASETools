c46402a0a16466722a9a192587848ef53b32bdae
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-10 18:41:35 -0700

    update

--- asetools/mod/pipeline/execute_step_super.py
+++ asetools/mod/pipeline/execute_step_super.py
@@ -63,8 +63,7 @@ class ExecutionStepSuper:
     def save_log(self):
         Log.info_chk(self.logger, msg_saving_run_info.format(PATH=self.log_name))
         log_json = self.get_log_json()
-        with open(self.get_log_path(), 'w') as out:
-            out.write(json.dumps(log_json, indent=4))
+        json.dump(log_json, self.get_log_path(), indent=4)
 
 
     def get_log_json(self, input_class_parse=FlagArg_to_tuple, output_class_parse=FlagArg_to_tuple):
