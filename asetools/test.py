c88af11f1b6b149344453d0175c7cf80c24daf42
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-10 17:03:54 -0700

    update

---
+++ asetools/test.py
@@ -0,0 +1,15 @@
+import sys
+from mod.pipeline.run.star_align import RunStarAlign
+
+def test_star_align():
+    star_align = RunStarAlign('tests/star_align_output_dir', 'examples/small.1.fastq', 'examples/small.2.fastq')
+    star_align.run()
+
+
+if __name__ == '__main__':
+
+    which = sys.argv[1]
+
+    if which == 'star_align':
+        test_star_align()
+
