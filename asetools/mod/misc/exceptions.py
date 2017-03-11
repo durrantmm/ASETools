9231fd85fa74a348abf012068365d9d63e937432
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-10 18:57:06 -0700

    update

--- asetools/mod/misc/exceptions.py
+++ asetools/mod/misc/exceptions.py
@@ -3,12 +3,12 @@ import sys
 
 class VersionError(Exception):
     def __init__(self, program, expected_version, observed_version):
-        self.message = "Specified {PROGRAM} is version {OBSERVED}, not {EXPECTED} as specified".format(
+        super().__init__("Specified {PROGRAM} is version {OBSERVED}, not {EXPECTED} as specified".format(
             PROGRAM = program, OBSERVED = observed_version, EXPECTED = expected_version
-        )
+        ))
 
 class ExecutionNotRanNoOutput(Exception):
     def __init__(self, program):
-        self.message = "The program {PROGRAM} has not yet been executed. No output path available.".format(
+        super().__init__("The program {PROGRAM} has not yet been executed. No output path available.".format(
             PROGRAM=program
-        )
+        ))
