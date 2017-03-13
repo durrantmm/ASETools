f1e5b2e919f2f609f2426bf8bd56b9cd7263936f
M Durrant <mdurrant@DN52emm0.SUNet>
2017-03-13 15:23:05 -0600

    update

--- asetools/mod/pipeline/run_python/steps/filter_vcf.py
+++ asetools/mod/pipeline/run_python/steps/filter_vcf.py
@@ -22,13 +22,32 @@ class RunFilterVCF(RunPythonStepSuper):
         self.biallelic_only = biallelic_only
         self.no_indels = no_indels
 
+        self.autosomal_chroms = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9'
+                                 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17',
+                                 'chr18', 'chr19', 'chr20', 'chr21', 'chr22']
+
 
     def process(self):
         reader = vcf.Reader(open(self.input),)
         writer = vcf.Writer(open(self.output, 'w'), reader)
 
-        for line in reader:
-            print(line)
+        for rec in reader:
+
+            if self.autosomal_only and rec.CHROM not in self.autosomal_chroms or \
+                            rec.CHROM not in [c.strip('chr') for c in self.autosomal_chroms]:
+                continue
+
+            if self.biallelic_only and not self.is_biallelic(rec):
+                continue
+
+
+
+    def is_biallelic(self, record):
+        print(record)
+
+
+
+
 
 
 
