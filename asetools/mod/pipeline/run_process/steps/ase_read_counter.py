700fe9d571ebd2768e739ce4dd9392a23ebd81a3
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-14 15:03:40 -0600

    update

--- asetools/mod/pipeline/run_process/steps/ase_read_counter.py
+++ asetools/mod/pipeline/run_process/steps/ase_read_counter.py
@@ -72,3 +72,5 @@ class RunGATKASEReadCounter(RunProcessStepSuper):
         self.java.check_version()
         super().check_version()
 
+    def execute_command(self, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False):
+        super().execute_command(stderr=subprocess.STDOUT)
