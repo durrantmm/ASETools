a7f9675a87ef4d57071ce4dcf9fd199534264d55
M Durrant <mdurrant@rescomp-16-331634.stanford.edu>
2017-03-14 15:09:29 -0600

    update

--- asetools/mod/pipeline/run_process/run_process_step_super.py
+++ asetools/mod/pipeline/run_process/run_process_step_super.py
@@ -67,7 +67,7 @@ class RunProcessStepSuper:
         self.ran = True
 
 
-    def execute_command(self, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False):
+    def execute_command(self, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False):
         command = self.format_command()
         Log.info_chk(self.logger, msg_executing_command.format(DELIM=NL, COMMAND=command))
         Log.debug_chk(self.logger, msg_execute_command_signature.format(STDERR=stderr, SHELL=shell))
