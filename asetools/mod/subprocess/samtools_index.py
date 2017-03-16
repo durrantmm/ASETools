cd242acc6bd077b5db25c18c97741b590479b700
M Durrant <mdurrant@DN0a22f59c.SUNet>
2017-03-16 17:29:10 -0600

    update

--- asetools/mod/subprocess/samtools_index.py
+++ asetools/mod/subprocess/samtools_index.py
@@ -35,6 +35,8 @@ class RunSamtoolsIndex(RunProcessStepSuper):
         # Adjusting attributes based on relevant input variables
         input.arg = input_bam
 
+        output.arg = input_bam
+
         super().__init__(name=name, output_dir=output_dir, execution_path=execution_path, version=version,
                          version_flag=version_flag, version_parser=version_parser, input=input,
                          output=output, args=args, log_name=log_name, logger=logger)
